bb4c7d14f7192b177a5a77137970dc8b5bdf7e5c
Aucante <sebastien.aucante@gmail.com>
2022-01-07 13:11:37 +0100

    Feat: Mdi

--- assets/plugins/vuetify.js
+++ assets/plugins/vuetify.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 import colors from 'vuetify/lib/util/colors';
 
@@ -8,6 +9,9 @@ import colors from 'vuetify/lib/util/colors';
 Vue.use(Vuetify)
 
 const opts = {
+    icons: {
+        iconfont: 'mdi', // default - only for display purposes
+    },
     theme: {
         themes: {
             light: {
